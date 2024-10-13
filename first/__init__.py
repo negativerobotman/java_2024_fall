aa445a39e4e81338a65cb05731bf1c4cd1db8908
negativerobotman <nukenovelaman@gmail.com>
2024-10-13 14:35:31 +0500

    Change __init__.py file

---
+++ first/__init__.py
@@ -0,0 +1,17 @@
+import check50
+
+@check50.check()
+def exists():
+	"""Are hello.py exist?"""
+	check50.exists("Hello.java")
+
+@check50.check(exists)
+def compiles():
+	"""Compiles 'Hello.java'"""
+	check50.run("javac Hello.java").exit(0)
+
+@check50.check(compiles)
+def prints_hello():
+	"""prints "hello, world\n" """
+	check50.run("java Hello").stdout("[Hh]ello, world!?\n", regex=True).exit(0)
+
